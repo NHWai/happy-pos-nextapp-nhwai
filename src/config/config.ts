35caac8ff6071fd356bb37e86e6683d135f28136
Naing Htet Wai <nayhtetwinee@gmail.com>
2023-05-30 12:06:37 +0630

    edit prisma schema

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,9 @@
 interface Config {
   baseurl: string;
+  secretJwt: string;
 }
 
 export const config: Config = {
-  baseurl: process.env.BASE_URL as string,
+  baseurl: process.env.NEXT_PUBLIC_BASE_URL as string,
+  secretJwt: process.env.SECRET_JWT as string,
 };
