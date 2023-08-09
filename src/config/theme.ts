41423a0a64120dd321a622babf820763d019230d
Naing Htet Wai <nayhtetwinee@gmail.com>
2023-08-09 11:37:05 +0630

    add color scheme for the project

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -2,9 +2,12 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
-    primary: { main: "#9EB23B", light: "#C7D36F" },
-    secondary: { main: "#E0DECA" },
-    info: { main: "#FCF9C6" },
+    primary: {
+      main: "#E86A33",
+      contrastText: "#FCF9C6",
+    },
+    secondary: { main: "#41644A", contrastText: "#C7D36F" },
+    info: { main: "#F2E3DB" },
   },
 });
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { MainLayout } from "@/components/MainLayout";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Paper, Stack, Typography } from "@mui/material";
 import RouterLink from "next/link";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
 import QrCode2Icon from "@mui/icons-material/QrCode2";
@@ -11,7 +11,9 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import Link from "next/link";
+import mypic from "../assets/logo.png";
+import Image from "next/image";
+import { Translate } from "@mui/icons-material";
 
 export default function Home() {
   return (
@@ -24,11 +26,32 @@ export default function Home() {
           padding: "1rem",
         }}
       >
+        <Image
+          src={mypic}
+          alt="Picture of the author"
+          width={120}
+          height={120}
+          style={{
+            // border: "1px solid red",
+            borderRadius: "50%",
+            transform: "translate(-20%, -20%)",
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+        />
         <Typography
           fontSize={{ xs: "2.125rem", sm: "3rem" }}
-          sx={{ marginBottom: "2.5rem" }}
+          sx={{
+            // marginTop: "5rem",
+            marginBottom: "2.5rem",
+            width: "100%",
+            textAlign: "center",
+          }}
+          fontWeight={"bold"}
+          color="primary.dark"
         >
-          Happy POS
+          Food4Life
         </Typography>
 
         <Box
@@ -44,6 +67,7 @@ export default function Home() {
           <Typography
             fontSize={{ xs: "1.1rem", sm: "1.3rem" }}
             textAlign={"center"}
+            color="secondary.light"
           >
             Manage your menu categlog easily with Happy POS and entice your
             customers with QR code ordering system.
@@ -69,14 +93,17 @@ export default function Home() {
             marginBottom: "3rem",
           }}
         >
-          <Typography fontSize={{ xs: "1.5rem", sm: "2.125rem" }}>
+          <Typography
+            fontSize={{ xs: "1.5rem", sm: "2.125rem" }}
+            color="primary.dark"
+          >
             Services We Provide
           </Typography>
 
           <RevealList
             interval={200}
             origin="bottom"
-            delay={500}
+            delay={100}
             duration={1000}
             className="services"
           >
@@ -86,8 +113,12 @@ export default function Home() {
               fontSize={"2.8rem"}
               gap="0.5rem"
             >
-              <MenuBookIcon fontSize="inherit" />
-              <Typography textAlign={"center"} variant="body2">
+              <MenuBookIcon fontSize="inherit" color="primary" />
+              <Typography
+                textAlign={"center"}
+                variant="body2"
+                color="secondary.light"
+              >
                 Easily manage your menus with Foodie POS{" "}
               </Typography>
             </Stack>
@@ -97,8 +128,12 @@ export default function Home() {
               fontSize={"2.8rem"}
               gap="0.5rem"
             >
-              <QrCode2Icon fontSize="inherit" />
-              <Typography textAlign={"center"} variant="body2">
+              <QrCode2Icon fontSize="inherit" color="primary" />
+              <Typography
+                textAlign={"center"}
+                variant="body2"
+                color="secondary.light"
+              >
                 Scan and Order. Quick and Easy. Your customers will love it.
               </Typography>
             </Stack>
@@ -108,8 +143,12 @@ export default function Home() {
               fontSize={"2.8rem"}
               gap="0.5rem"
             >
-              <AddLocationAltIcon fontSize="inherit" />
-              <Typography textAlign={"center"} variant="body2">
+              <AddLocationAltIcon fontSize="inherit" color="primary" />
+              <Typography
+                textAlign={"center"}
+                variant="body2"
+                color="secondary.light"
+              >
                 Happy POS supports multiple locations for your bussiness.
               </Typography>
             </Stack>
@@ -119,14 +158,27 @@ export default function Home() {
               fontSize={"2.8rem"}
               gap="0.5rem"
             >
-              <CardMembershipIcon fontSize="inherit" />
-              <Typography textAlign={"center"} variant="body2">
+              <CardMembershipIcon fontSize="inherit" color="primary" />
+              <Typography
+                textAlign={"center"}
+                variant="body2"
+                color="secondary.light"
+              >
                 Backoffice and Order App are included in every subscription.
               </Typography>
             </Stack>
-            <Stack sx={{ maxWidth: "200px" }} alignItems={"center"}>
-              <SupportAgentIcon fontSize="large" />
-              <Typography textAlign={"center"} variant="body2">
+            <Stack
+              sx={{ maxWidth: "200px" }}
+              alignItems={"center"}
+              fontSize={"2.8rem"}
+              gap="0.5rem"
+            >
+              <SupportAgentIcon fontSize="inherit" color="primary" />
+              <Typography
+                textAlign={"center"}
+                variant="body2"
+                color="secondary.light"
+              >
                 Dedicated customer support so that we are always here to help
                 you.
               </Typography>
@@ -136,7 +188,7 @@ export default function Home() {
       </Box>
       <Box
         sx={{
-          backgroundColor: "#4682A9",
+          backgroundColor: "secondary.light",
           width: "100%",
           padding: "3rem 0 3rem",
           color: "#F6F4EB",
@@ -190,6 +242,39 @@ export default function Home() {
           </Box>
         </Box>
       </Box>
+      <svg
+        className="blob-background"
+        viewBox=" 0 0 200 200"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill="#F2E3DB"
+          d="M69.5,-24.3C75.9,-3,57,24.8,35,38.5C13,52.2,-12.2,51.8,-28.9,39.7C-45.6,27.6,-53.8,3.7,-47.6,-17.4C-41.4,-38.4,-20.7,-56.7,5.4,-58.5C31.6,-60.3,63.1,-45.5,69.5,-24.3Z"
+          transform="translate(280 50)"
+        />
+      </svg>
+      <svg
+        className="blob-background"
+        viewBox="90 0 200 200"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill="#F2E3DB"
+          d="M46.1,-58.1C50.5,-51.2,38.5,-27.9,35.2,-11.4C31.8,5.1,37.2,14.8,36.1,24.8C35,34.7,27.5,44.9,18,47.3C8.6,49.8,-2.9,44.6,-17.1,41.7C-31.4,38.9,-48.4,38.3,-61.1,29.1C-73.7,19.9,-82,2,-79,-13.7C-76,-29.5,-61.7,-43,-46.7,-48.1C-31.6,-53.2,-15.8,-49.8,2.5,-52.8C20.8,-55.8,41.6,-65.1,46.1,-58.1Z"
+          transform="translate(20 150)"
+        />
+      </svg>
+      <svg
+        className="blob-background"
+        viewBox="0 0 200 200"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill="#F2E3DB"
+          d="M37.3,25.4C23.2,45.9,-30.6,47.3,-43.4,27.5C-56.2,7.8,-28.1,-33.1,-1.2,-33.8C25.7,-34.5,51.3,5,37.3,25.4Z"
+          transform="translate(100 100)"
+        />
+      </svg>
     </MainLayout>
   );
 }
