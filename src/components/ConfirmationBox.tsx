de00039349f02be279d35efeb76669ff9f4993be
Naing Htet Wai <nayhtetwinee@gmail.com>
2023-08-09 22:22:04 +0630

    remove mui joy library and creat modal box with mui

--- src/components/ConfirmationBox.tsx
+++ src/components/ConfirmationBox.tsx
@@ -1,8 +1,9 @@
 import BackOfficeContext from "@/contexts/BackofficeContext";
-import { ModalClose, ModalDialog, ModalOverflow } from "@mui/joy";
-import Modal from "@mui/joy/Modal";
-import { Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import React, { useContext } from "react";
+
+import Modal from "@mui/material/Modal";
+import CancelPresentationIcon from "@mui/icons-material/CancelPresentation";
 
 interface Props {
   open: boolean;
@@ -10,6 +11,17 @@ interface Props {
   handleDelete: () => void;
   heading: string;
 }
+
+const style = {
+  backgroundColor: "white",
+  padding: "1.5rem",
+  borderRadius: "1rem",
+  minWidth: "200px",
+  maxWidth: "800px",
+  display: "flex",
+  flexDirection: "column",
+  // alignItems: "space-between",
+};
 
 const ConfirmationBox = ({ open, setOpen, heading, handleDelete }: Props) => {
   const { app } = useContext(BackOfficeContext);
@@ -24,38 +36,48 @@ const ConfirmationBox = ({ open, setOpen, heading, handleDelete }: Props) => {
           alignItems: "center",
         }}
       >
-        <ModalOverflow>
-          <ModalDialog layout="center" size="lg">
-            <ModalClose />
-            <Typography
-              my={2}
-              align="center"
-              variant="caption"
-              sx={{ maxWidth: 300 }}
+        <Box sx={style}>
+          <Typography
+            onClick={() => setOpen(false)}
+            color="error"
+            sx={{
+              padding: "0px !important",
+              margin: "0px !important",
+              display: "inline-flex",
+              justifyContent: "right",
+              cursor: "pointer",
+            }}
+          >
+            <CancelPresentationIcon />
+          </Typography>
+          <Typography
+            my={2}
+            align="center"
+            variant="body1"
+            sx={{ maxWidth: "700px" }}
+          >
+            {heading}
+          </Typography>
+          <Stack direction={"row"} justifyContent={"flex-end"} gap={1}>
+            <Button
+              disabled={app.status === "loading"}
+              onClick={handleDelete}
+              size="small"
+              variant="outlined"
+              color="error"
             >
-              {heading}
-            </Typography>
-            <Stack direction={"row"} justifyContent={"flex-end"} gap={1}>
-              <Button
-                disabled={app.status === "loading"}
-                onClick={handleDelete}
-                size="small"
-                variant="outlined"
-                color="error"
-              >
-                Delete
-              </Button>
-              <Button
-                disabled={app.status === "loading"}
-                onClick={() => setOpen(false)}
-                size="small"
-                variant="outlined"
-              >
-                Cancel
-              </Button>
-            </Stack>
-          </ModalDialog>
-        </ModalOverflow>
+              Delete
+            </Button>
+            <Button
+              disabled={app.status === "loading"}
+              onClick={() => setOpen(false)}
+              size="small"
+              variant="outlined"
+            >
+              Cancel
+            </Button>
+          </Stack>
+        </Box>
       </Modal>
     </div>
   );
--- src/components/ModalBox.tsx
+++ src/components/ModalBox.tsx
@@ -1,7 +1,8 @@
-import { ModalClose, ModalDialog, ModalOverflow } from "@mui/joy";
-import Modal from "@mui/joy/Modal";
-import { Typography } from "@mui/material";
+// import { ModalClose, ModalDialog, ModalOverflow } from "@mui/joy";
+// import Modal from "@mui/joy/Modal";
+import { Box, Modal, Typography } from "@mui/material";
 import React from "react";
+import CancelPresentationIcon from "@mui/icons-material/CancelPresentation";
 
 interface Props {
   children: React.ReactNode;
@@ -10,30 +11,54 @@ interface Props {
   heading: string;
 }
 
+const style1 = {
+  backgroundColor: "white",
+  padding: "1rem",
+  borderRadius: "1rem",
+  width: "fit-content",
+  minWidth: "240px",
+  maxWidth: "800px",
+  display: "flex",
+  flexDirection: "column",
+  maxHeight: "80vh",
+  overflowY: "auto",
+  scrollbarWidth: "none", // Hide scrollbar on Firefox
+  "&::-webkit-scrollbar": {
+    display: "none", // Hide scrollbar on Chrome, Safari, and Edge
+  },
+};
+
 const ModalBox = ({ children, open, setOpen, heading }: Props) => {
   return (
-    <>
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          overflow: "auto",
-        }}
-      >
-        <ModalOverflow>
-          <ModalDialog layout="center" size="lg">
-            <ModalClose />
-            <Typography my={2} align="center" variant="h5">
-              {heading}
-            </Typography>
-            {children}
-          </ModalDialog>
-        </ModalOverflow>
-      </Modal>
-    </>
+    <Modal
+      open={open}
+      onClose={() => setOpen(false)}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box sx={style1}>
+        <Typography
+          onClick={() => setOpen(false)}
+          color="error"
+          sx={{
+            padding: "0px !important",
+            margin: "0px !important",
+            display: "inline-flex",
+            justifyContent: "right",
+            cursor: "pointer",
+          }}
+        >
+          <CancelPresentationIcon />
+        </Typography>
+        <Typography my={2} align="center" variant="h5">
+          {heading}
+        </Typography>
+        {children}
+      </Box>
+    </Modal>
   );
 };
 
